3c7808c7656e371c012146ba01c5f88080391555
Angel Lopez <alopez.215.95@gmail.com>
2024-01-03 19:17:54 -0400

    add icons material

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,12 +4,21 @@ import App from './App.vue'
 import router from './router'
 
 // Vuetify
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, md } from 'vuetify/iconsets/md'
 
 const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'md',
+        aliases,
+        sets: {
+          md,
+        },
+    },
     components,
     directives,
 })
